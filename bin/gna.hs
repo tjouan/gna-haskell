15075bc3e7b522b8afd81f222d9176c1f62ce100
Thibault Jouan <tj@a13.fr>
2014-01-11 01:32:00 +0000

    Implement actions dispatch

--- bin/gna.hs
+++ bin/gna.hs
@@ -1,13 +1,37 @@
 #!/usr/bin/env runhaskell
 
+import Data.List
 import System.Environment
 import System.FilePath.Glob
 
-findRepos :: [Char] -> IO [FilePath]
-findRepos path = globDir1 (compile "**/.git") path
+
+dispatch :: [(String, [String] -> IO ())]
+dispatch = [
+  ("discover",  discover),
+  ("list",      list),
+  ("check",     check)
+  ]
+
 
 main = do
-  args <- getArgs
-  repos <- findRepos $ args !! 0
+  (command:args) <- getArgs
+  let (Just action) = lookup command dispatch
+  action args
 
+
+discover :: [String] -> IO ()
+discover [path] = do
+  repos <- findRepos path
   putStrLn $ show repos
+
+
+list :: [String] -> IO ()
+list [] = putStrLn "list"
+
+
+check :: [String] -> IO ()
+check [] = putStrLn "check"
+
+
+findRepos :: [Char] -> IO [FilePath]
+findRepos path = globDir1 (compile "**/.git") path
