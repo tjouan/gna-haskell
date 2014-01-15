b68b4d95252d3048339cbd849143d14ec9075ca6
Thibault Jouan <tj@a13.fr>
2014-01-15 23:21:11 +0000

    Save RC file in home directory

--- bin/gna.hs
+++ bin/gna.hs
@@ -1,12 +1,12 @@
 #!/usr/bin/env runhaskell
 
-import System.Environment (getArgs)
+import System.Environment (getArgs, getEnv)
 import System.FilePath.Glob (compile, globDir1)
 import System.IO (appendFile)
 
 
-rcFilePath :: String
-rcFilePath = ".gna.rc"
+rcFileName :: String
+rcFileName = ".gna.rc"
 
 
 dispatch :: [(String, [String] -> IO ())]
@@ -39,5 +39,12 @@ check [] = putStrLn "check"
 findRepos :: String -> IO [FilePath]
 findRepos = globDir1 $ compile "**/.git"
 
+rcFilePath :: IO String
+rcFilePath = do
+  home <- getEnv "HOME"
+  return (home ++ "/" ++ rcFileName)
+
 saveRCFile :: String -> IO ()
-saveRCFile content = writeFile rcFilePath content
+saveRCFile content = do
+  path <- rcFilePath
+  writeFile path content
